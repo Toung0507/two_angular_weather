704a03423935d1ab6777b54a9aab61295ed3599a
Toung-test <C108193246@nkust.edu.tw>
2025-12-09 14:32:10 +0800

    在所需頁面設定好 httpService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpService } from './@http-service/http.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'weatherApi';
+  constructor(private httpService: HttpService) { }
+
+  url: string = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-D0047-065?Authorization=CWA-586EED3D-A84F-437E-9109-E4D418BC7A2D";
+
 }
